848e150a5bbf9966870d61fd415e8a0c7c8019a8
Parag Tiwari <paragt2005@gmail.com>
2026-03-17 12:54:33 +0530

    Time: 25 ms (52.52%) | Memory: 149.1 MB (88.00%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,21 +1,33 @@
 class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
 public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row>0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + 
-                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+    NumMatrix(vector<vector<int>>& matrix) {
+        precompute = vector<vector<int>>(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
+
+        for (int row = 0; row < matrix.size(); row++) {
+            int rowSum = 0;
+            for (int col = 0; col < matrix[0].size(); col++) {
+                precompute[row + 1][col + 1] = precompute[row][col + 1] + rowSum + matrix[row][col];
+                rowSum += matrix[row][col];
             }
         }
-    }
 
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+        // for (int i = 0; i <= matrix.size(); i++) {
+        //     for (int j = 0; j <= matrix[0].size(); j++) {
+        //         cout << precompute[i][j] << "\t";
+        //     }
+        //     cout << endl;
+        // }
     }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return precompute[row2 + 1][col2 + 1] + precompute[row1][col1] - precompute[row1][col2 + 1] - precompute[row2 + 1][col1];
+    }
+private:
+    vector<vector<int>> precompute;
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */

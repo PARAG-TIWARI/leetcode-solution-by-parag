c3c22ffc38a2d01b541d77b866c788f37a636e9e
Parag Tiwari <paragt2005@gmail.com>
2026-04-17 23:35:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.21%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // negative or ends with 0 (but not 0 itself)
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        // for even digits: x == rev
+        // for odd digits: x == rev/10
+        return (x == rev || x == rev / 10);
+    }
+};
